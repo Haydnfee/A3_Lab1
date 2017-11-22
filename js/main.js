a9690948b8e8a0fac3ec2f9e61d1de492836be32
Fee <h_fee@tss-hr420-dm18.fc.ca>
2017-11-22 16:49:38 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
+   var carSelect = document.querySelectorAll('.data-ref'),
+   var carModel = document.querySelector('.modelName'),
+   var carPrice = document.querySelector('.priceInfo'),
+   var carDesc = document.querySelector('.modelDetails'),
+
+   function highlightSwap
+   
+
+})();
