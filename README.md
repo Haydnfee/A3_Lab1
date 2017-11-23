01858bc251d10036b5a337410fdd356e548a9960
haydnfee <hfeegdci@gmail.com>
2017-11-22 20:56:42 -0500

    Trying to get JS to work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A3_Lab1
+
+I just don't understand JS.
--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
    var carSelect = document.querySelectorAll('.data-ref'),
-   var carModel = document.querySelector('.modelName'),
-   var carPrice = document.querySelector('.priceInfo'),
-   var carDesc = document.querySelector('.modelDetails'),
+    carModel = document.querySelector('.modelName h4'),
+    carPrice = document.querySelector('.priceInfo p'),
+    carDesc = document.querySelector('.modelDetails p'),
 
-   function highlightSwap
+    function changeName() {
+    document.querySelector(".modelname").innerHTML = '';
+    console.log('fired');
+   }
    
+   document.querySelector(carSelect).addEventListener("click", changeName, false)
 
 })();
